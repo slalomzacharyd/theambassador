3b7c24fa9acd1356685e3b54cdff646c63b72359
Zachary Dovel <zacharyd@slalom.com>
2015-05-13 11:35:45 -0700

    Small fixes

--- docker/setup.rb
+++ docker/setup.rb
@@ -32,11 +32,11 @@ Dir.chdir(cwd)
 #Create the init script
 service_template = ERB.new(File.read('kegbot.service.erb'))
 service = service_template.result()
+FileUtils.mkdir_p "/usr/lib/systemd/system"
 File.write("/usr/lib/systemd/system/kegbot-#{server_name}.service", service)
 
 script_template = ERB.new(File.read('kegbot.prescript.erb'))
 script = script_template.result()
-FileUtils.mkdir_p "/usr/lib/systemd/scripts/kegbot-#{server_name}-prestart"
+FileUtils.mkdir_p "/usr/lib/systemd/scripts"
 File.write("/usr/lib/systemd/scripts/kegbot-#{server_name}-prestart", script)
-FileUtils.mkdir_p "/usr/lib/systemd/scripts/kegbot-#{server_name}-prestart"
 File.chmod(0755, "/usr/lib/systemd/scripts/kegbot-#{server_name}-prestart")
